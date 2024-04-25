7948857f18ca5574f4d1b8e5390908cae66ab684
Edgardo Ponce <ed.poncepaez@gmail.com>
2024-04-25 13:13:38 -0300

    adding code example from platzi

---
+++ src/app/main-test.ts
@@ -0,0 +1,18 @@
+class Data {
+
+    name: string;
+    a: string;
+    b: string;
+    c: string;
+
+    constructor(name: string,a: string, b:string, c:string){
+        this.name = name;
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+}
+
+const a = new Data('name','a','b','c');
+
+
